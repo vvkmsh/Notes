1a89d171f58110d65527139cd563855daa280982
vvkmsh <40882299+vvkmsh@users.noreply.github.com>
2018-07-06 12:27:57 +0530

    Add files via upload

---
+++ 04_SetOutFile.js
@@ -0,0 +1,7 @@
+var outFile = "C:\\Temp\\EmpPEx_TPAfirm_BrokerDeal_v150713.txt";
+var str = "";
+var partID = "";
+var plan = ""
+
+var str = TSnow() + "TestIterationStart" + "\r\n" ;
+IO.write(outFile,str, true, "UTF-8");
